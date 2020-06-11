31fbf44e387551ddc901634d6a2c9cbbf2b50868
Florian Klampfer <mail@qwtel.com>
2020-06-11 12:48:02 +0700

    1.0.0-alpha.0

---
+++ lib/util.d.ts
@@ -0,0 +1,6 @@
+export declare function getScrollHeight(): number;
+export declare function getScrollLeft(): number;
+export declare function getScrollTop(): number;
+export declare const matches: (el: Element, selector: string) => any;
+export declare function matchesAncestors(el: Element, selector: string): Element | null;
+export declare function fragmentFromString(strHTML: string): DocumentFragment;
