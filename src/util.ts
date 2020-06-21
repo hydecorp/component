3d61fc1b8ea18ba40a5ee4ffe9858b667ecbd9b8
Florian Klampfer <mail@qwtel.com>
2020-06-21 21:42:12 +0700

    Add resolvable promise

--- src/util.ts
+++ src/util.ts
@@ -28,3 +28,15 @@ export function matchesAncestors(el: Element, selector: string): Element | null 
 export function fragmentFromString(strHTML: string): DocumentFragment {
   return document.createRange().createContextualFragment(strHTML);
 }
+
+type Resolver<T> = (value: T | PromiseLike<T>) => void;
+type Rejector = (reason?: any) => void;
+type ResolvablePromise<T> = Promise<T> & { resolve: Resolver<T>, reject: Rejector }
+export function createResolvablePromise<T>(): ResolvablePromise<T> {
+  let res: Resolver<T>
+  let rej: Rejector;
+  const promise = new Promise((r, s) => (res = r, rej = s)) as ResolvablePromise<T>;
+  promise.resolve = res;
+  promise.reject = rej;
+  return promise;
+}
