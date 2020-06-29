d7c43568a3f09b45c8fc5903a2503b7c2d417b6c
Florian Klampfer <mail@qwtel.com>
2020-06-29 14:54:44 +0700

    Restore preversion .gitignore

--- lib/observers.d.ts
+++
@@ -1,5 +0,0 @@
-import { Observable } from "rxjs";
-export declare function createResizeObservable(el: HTMLElement): Observable<ResizeObserverEntry>;
-export declare function createMutationObservable(el: HTMLElement, options?: MutationObserverInit): Observable<MutationRecord>;
-export declare function createIntersectionObservable(els: HTMLElement | HTMLElement[], options?: IntersectionObserverInit): Observable<IntersectionObserverEntry[]>;
-//# sourceMappingURL=observers.d.ts.map
--- lib/operators.d.ts
+++
@@ -1,6 +0,0 @@
-import { Observable, OperatorFunction } from "rxjs";
-export declare function subscribeWhen<T>(p$: Observable<boolean>): (source: Observable<T>) => Observable<T>;
-export declare function unsubscribeWhen<T>(p$: Observable<boolean>): (source: Observable<T>) => Observable<T>;
-export declare function filterWhen<T>(p$: Observable<boolean>, ...others: Observable<boolean>[]): (source: Observable<T>) => Observable<T>;
-export declare function bufferDebounceTime<T>(time?: number): OperatorFunction<T, T[]>;
-//# sourceMappingURL=operators.d.ts.map
